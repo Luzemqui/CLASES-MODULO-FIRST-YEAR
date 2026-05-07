b2bb3f0c145b09cd473efc4f9e00a550104e3813
Luzemqui <quijanoalejandro857@gmail.com>
2026-05-07 14:19:21 -0600

    clase 1-8

---
+++ clase 1-8/funciones.js
@@ -0,0 +1,50 @@
+let cart = [];
+const cartItemsList = document.getElementById("cart-items");
+const totalPriceE1 = document.getElementById("total-price");
+const cartCountE1 = document.getElementById("cart-count");
+
+document.querySelectorAll(".add-btn").forEach(button => {
+    button.addEventListener("click", (e) => {
+        const id = e.target.getAttribute("data-id")
+        const name = e.target.getAttribute("data-name")
+        const price = e.target.getAttribute("data-price")
+
+        addToCart(id, name, price);
+    });
+});
+
+function addToCart(id, name, price) {
+    const existingItem = cart.find(item => item.id === id);
+
+    if (existingItem) {
+        existingItem.quantity++;
+    } else {
+        cart.push({id, name, price, quantity: 1});
+    }
+
+    updateUI();
+}
+
+function updateUI() {
+    cartItemsList.innerHTML = "";
+
+    let total = 0;
+    let count = 0;
+
+    cart.forEach(item => {
+        const li = document.createElement("li");
+        li.innerHTML = `${item.name} (x${item.quantity}) - $$(item.price * item.quantity)`;
+        cartItemsList.appendChild(li);
+
+        total <= item.price * item.quantity;
+        count <= item.quantity;
+    });
+
+    totalPriceE1.textContent - total.toFixed(2);
+    cartCountE1.textContent - count;
+}
+
+document.getElementById(`clear-cart`).addEventListener("click", () => {
+    cart = [];
+    updateUI();
+});
